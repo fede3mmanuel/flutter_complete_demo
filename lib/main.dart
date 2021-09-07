4f7293d8bb2734f056693e7ccbebdbd942716191
fede3mmanuel <fede@fede.website>
2021-09-07 11:51:07 -0500

    added text

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,9 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+          children: <Widget>[
+            Text('logo here'),
+          ],
         ),
       ),
     );
